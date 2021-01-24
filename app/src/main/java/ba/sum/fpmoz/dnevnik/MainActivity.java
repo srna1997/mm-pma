c3ca9cfbf80e578be4b9ac39cd1fab424c1553ab
Ermin <erminsrna17@gmail.com>
2021-01-24 17:14:00 +0100

    eDnevnik-java

--- app/src/main/java/ba/sum/fpmoz/dnevnik/MainActivity.java
+++ app/src/main/java/ba/sum/fpmoz/dnevnik/MainActivity.java
@@ -14,6 +14,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText emailInp;
     private EditText passwordInp;
     private Button loginBtn;
-    private Button registerBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,45 +34,49 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
 
-        /*Dohvacanje elemenata sucelja*/
         this.messageTxt = findViewById(R.id.messageTxt);
         this.emailInp = findViewById(R.id.emailInp);
         this.passwordInp = findViewById(R.id.passwordInp);
         this.loginBtn = findViewById(R.id.loginBtn);
-        this.registerBtn = findViewById(R.id.registerBtn);
 
-        /*Na klik dohvati unos korisnika i probaj autenticirati*/
         this.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailInp.getText().toString();
                 String password = passwordInp.getText().toString();
 
-                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            messageTxt.setText("Uspješno ste se prijavili na sustav!");
-                            Intent I = new Intent(getApplicationContext(),HomeActivity.class);
-                            startActivity(I);
-                        }
-                        else {
-                            System.out.println(task.getException());
-                            messageTxt.setText("Nastao je problem sa prijavom");
+                        if (task.isSuccessful()) {
+                            messageTxt.setText("Uspješno ste se prijavili na sustav.");
+                            String uId = mAuth.getCurrentUser().getUid();
+                            FirebaseDatabase db = FirebaseDatabase.getInstance();
+                            DatabaseReference ref = db.getReference("ednevnik/korisnici/"+uId);
+                            ref.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if(snapshot.child("role").getValue().toString().equals("Admin")){
+                                        startActivity(new Intent(getApplicationContext(), HomeNavigationActivity.class));
+                                    } else if(snapshot.child("role").getValue().toString().equals("Nastavnik")){
+                                        startActivity(new Intent(getApplicationContext(), TeacherMainActivity.class));
+                                    } else{
+                                        startActivity(new Intent(getApplicationContext(), StudentMainActivity.class));
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
                         }
                     }
                 });
-
             }
-        });
 
-        /* Na klik posalji na register */
-        this.registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),RegisterActivity.class);
-                startActivity(i);
-            }
+
         });
     }
 }
